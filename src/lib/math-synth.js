533e104ac725d8a5383b6585faade6091e6376c9
caesarsol <smilemachine@gmail.com>
2017-06-13 03:37:45 +0200

    📊 Solved frequency visualization (thanks Fourier!)

--- src/lib/math-synth.js
+++ src/lib/math-synth.js
@@ -1,5 +1,8 @@
+import FFT from 'lib/nayuki-fft'
+
 const context = new window.AudioContext()
 const TWO_PI = 2 * Math.PI
+const MAX_AUDIBLE_FREQ = 20000
 
 function sinPi(x) { return Math.sin(x * TWO_PI) }
 function clamp(x, [min, max]) { return Math.max(Math.min(x, max), min) }
@@ -62,26 +65,51 @@ export function drawBufData(audioData, givenCanvas = null) {
   ctx.fillStyle = 'steelblue'
   ctx.globalAlpha = 0.4
   ctx.fillRect(0, 0, w, h)
-  ctx.globalAlpha = 1.0
+  ctx.beginPath()
   for (let x = 0; x < w; x++) {
-    const y = (audioData[x] / 2 + 0.5) * h
-    ctx.fillRect(x, h - y - 1, 1, 2)
+    const y = (audioData[x] * 0.5 + 0.5) * h
+    ctx.lineTo(x, h - y)
     ctx.globalAlpha = 0.3
     ctx.fillRect(x, h - y, 1, y)
-    ctx.globalAlpha = 1.0
   }
-
-  function fft(signal) { return Array(1024).fill(0).map((x, i) => Math.sin(x)) }
-  const fourierData = fft(audioData)
-  ctx.fillStyle = 'tomato'
   ctx.globalAlpha = 1.0
-  for (let x = 0; x < fourierData.length; x++) {
-    const y = fourierData[x] * h
-    ctx.fillRect(x, h - y - 1, 1, 2)
-    ctx.globalAlpha = 0.3
-    ctx.fillRect(x, h - y, 1, y)
-    ctx.globalAlpha = 1.0
+  ctx.strokeStyle = 'steelblue'
+  ctx.lineWidth = 2
+  ctx.stroke()
+
+  const n = 1024 * 8
+  const fft = new FFT(n)
+  const fourierDataReal = audioData.slice(0, n)
+  const fourierDataImag = fourierDataReal.map(_ => 0)
+  fft.forward(fourierDataReal, fourierDataImag)
+  // https://stackoverflow.com/questions/4364823/how-do-i-obtain-the-frequencies-of-each-value-in-an-fft#answer-4371627
+  const lm = fourierDataReal.length / 2 // For a Real input signal the second half of the FFT contain no useful additional information.
+  const fb = context.sampleRate / n // frequencies bin
+  const l = Math.min(MAX_AUDIBLE_FREQ / fb, lm)
+  const freqPerPixel = 0.397 // Arbitrary. TODO find the formula
+  const bw = fb * freqPerPixel // bin width
+  ctx.beginPath()
+  for (let k = 0; k < l; k++) {
+    const frequency = k * fb
+    const amplitude = Math.sqrt(fourierDataReal[k] ** 2 + fourierDataImag[k] ** 2)
+
+    const x = frequency * freqPerPixel
+    const y = amplitude / 20
+    ctx.lineTo(x, h - y)
+    ctx.globalAlpha = 0.6
+    ctx.fillStyle = 'tomato'
+    ctx.fillRect(x, h - y, bw, y)
+
+    // if (k > 0 && Math.ceil(frequency) % 440 < fb) {
+    //   ctx.globalAlpha = 1.0
+    //   ctx.fillStyle = 'yellow'
+    //   ctx.fillRect(x, 0, bw, h)
+    // }
   }
+  ctx.globalAlpha = 1.0
+  ctx.strokeStyle = 'tomato'
+  ctx.lineWidth = 2
+  ctx.stroke()
 
   if (!givenCanvas) document.body.appendChild(canvas)
   return canvas
